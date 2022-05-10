05e3bb05ded40f7703fc75e6d900287af5183537
Sira-Unudomrak <59911623+art44903@users.noreply.github.com>
2022-05-10 18:02:16 +0700

    update

--- frontend/src/views/Home-page.vue
+++ frontend/src/views/Home-page.vue
@@ -2,7 +2,7 @@
   <body>
     <div class="text-center">
       <b-img class="text-center" src="" fluid alt="Responsive image"></b-img><br><br>
-      <h1>ดีแทค เปิดลงทะเบียน รับการฉีดวัคซีนโควิด-19</h1><br>
+      <h1>เปิดลงทะเบียน รับการฉีดวัคซีนโควิด-19</h1><br>
       <a href="/register" class="btn btn-red" role="button">ลงทะเบียนจองคิว - Register</a>
     </div>
   </body>
