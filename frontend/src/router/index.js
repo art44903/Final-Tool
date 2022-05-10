fa42f52b8cc3f8ca0f5ff3490c1a04fe9fd6e60b
Sira-Unudomrak <59911623+art44903@users.noreply.github.com>
2022-05-10 17:49:32 +0700

    update

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,26 +1,58 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-];
+    {
+        path: '/',
+        name: 'vaccinehome',
+        component: () => import('../views/Home-page.vue')
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: () => import('../views/Register-page.vue')
+    },
+    {
+        path: '/confirm',
+        name: 'confirm',
+        component: () => import('../views/Confirm-page.vue')
+    }
+]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
 
-export default router;
+export default router
+
+// import { createRouter, createWebHistory } from "vue-router";
+
+
+// const routes = [
+//     {
+//         path: '/',
+//         name: 'vaccinehome',
+//         component: () => import('../views/Home-page.vue')
+//     },
+//     {
+//         path: '/register',
+//         name: 'register',
+//         component: () => import('../views/Register-page.vue')
+//     },
+//     {
+//         path: '/confirm',
+//         name: 'confirm',
+//         component: () => import('../views/Confirm-page.vue')
+//     }
+// ];
+
+// const router = createRouter({
+//   history: createWebHistory(process.env.BASE_URL),
+//   routes,
+// });
+
+// export default router;
---
+++ frontend/src/views/Home-page.vue
@@ -0,0 +1,14 @@
+<template>
+  <body>
+    <div class="text-center">
+      <b-img class="text-center" src="https://app.dtac.co.th/vaccine/assets/images/banner/WebBanner-desktop-1120x400-Th_2022-Apr-28.jpg" fluid alt="Responsive image"></b-img><br><br>
+      <h1>ดีแทค เปิดลงทะเบียน รับการฉีดวัคซีนโควิด-19</h1><br>
+      <a href="/register" class="btn btn-primary" role="button">ลงทะเบียนจองคิว - Register</a>
+    </div>
+  </body>
+</template>
+
+<script>
+
+
+</script>
